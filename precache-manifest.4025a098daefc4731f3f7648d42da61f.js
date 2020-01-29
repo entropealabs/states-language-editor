e0f59253f54e8cce01af1c382c69cad5365b43fd
Christopher <chris@entropealabs.com>
2020-01-29 16:32:52 -0600

    Updates

---
+++ precache-manifest.4025a098daefc4731f3f7648d42da61f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc21eda6cea325e15464d04632a1abf",
+    "url": "/states-language-editor/index.html"
+  },
+  {
+    "revision": "fd83fc4084a253f6ea86",
+    "url": "/states-language-editor/static/css/main.d1bf1e66.chunk.css"
+  },
+  {
+    "revision": "c13edcd9369756a09c55",
+    "url": "/states-language-editor/static/js/2.523df81b.chunk.js"
+  },
+  {
+    "revision": "fd83fc4084a253f6ea86",
+    "url": "/states-language-editor/static/js/main.30093d70.chunk.js"
+  },
+  {
+    "revision": "c1cc07d966319444ce4c",
+    "url": "/states-language-editor/static/js/runtime~main.012934cc.js"
+  }
+]);
